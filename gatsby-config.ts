511e840ab75f677004deb0216b40124cdc4a175a
partykyoung <partykyoung@gmail.com>
2023-11-08 17:28:40 +0900

    about 페이지 업데이트

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -15,7 +15,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
+        icon: 'src/assets/images/icon.png',
       },
     },
     'gatsby-plugin-mdx',
@@ -25,7 +25,7 @@ const config: GatsbyConfig = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: './src/images/',
+        path: './src/assets/images/',
       },
       __key: 'images',
     },
