fd805af3d5f6c321a3b6d7f98f75ea407813bfc2
partykyoung <partykyoung@gmail.com>
2024-02-19 11:02:12 +0900

    chore: 설정 변경 - gatsby config 설정 이전 설정에 맞게 수정 - 개발 모드 실행시 발생하는 이슈들 수정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import fs from 'fs';
 
 import type { GatsbyConfig } from 'gatsby';
@@ -19,18 +20,39 @@ const config: GatsbyConfig = {
     title: `dev-blog`,
     siteUrl: `https://dev.kyoungah.me`,
 =======
+=======
+import fs from "fs";
+
+>>>>>>> b6708e4 (chore: 설정 변경)
 import type { GatsbyConfig } from "gatsby";
+
+// 페이징에 필요한 폴더 및 json 파일을 만든다.
+function createFolder(folderName: string) {
+  const dir = `${__dirname}/static/${folderName}`;
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+}
+
+createFolder("jsons");
+createFolder("posts");
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Kyoungah Dev Blog`,
+<<<<<<< HEAD
     siteUrl: `https://www.yourdomain.tld`
 >>>>>>> ce459de (Initial commit from gatsby)
+=======
+    siteUrl: `https://dev.kyoungah.me`,
+>>>>>>> b6708e4 (chore: 설정 변경)
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+<<<<<<< HEAD
 <<<<<<< HEAD
   plugins: [
     'gatsby-plugin-emotion',
@@ -118,17 +140,61 @@ const config: GatsbyConfig = {
     options: {
       "name": "images",
       "path": "./src/images/"
+=======
+  plugins: [
+    "gatsby-plugin-postcss",
+    // "gatsby-plugin-google-gtag",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+>>>>>>> b6708e4 (chore: 설정 변경)
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
+<<<<<<< HEAD
     __key: "pages"
   }]
 >>>>>>> ce459de (Initial commit from gatsby)
+=======
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/posts/`,
+      },
+      __key: "posts",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/static/images/`,
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "jsons",
+        path: `${__dirname}/static/jsons/`,
+      },
+      __key: "jsons",
+    },
+  ],
+>>>>>>> b6708e4 (chore: 설정 변경)
 };
 
 export default config;
