b6708e4456af0eeb3d6e27e540ef1c74772c8ffe
partykyoung <partykyoung@gmail.com>
2024-02-19 11:02:12 +0900

    chore: 설정 변경 - gatsby config 설정 이전 설정에 맞게 수정 - 개발 모드 실행시 발생하는 이슈들 수정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,34 +1,75 @@
+import fs from "fs";
+
 import type { GatsbyConfig } from "gatsby";
+
+// 페이징에 필요한 폴더 및 json 파일을 만든다.
+function createFolder(folderName: string) {
+  const dir = `${__dirname}/static/${folderName}`;
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+}
+
+createFolder("jsons");
+createFolder("posts");
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Kyoungah Dev Blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://dev.kyoungah.me`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    // "gatsby-plugin-google-gtag",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/posts/`,
+      },
+      __key: "posts",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/static/images/`,
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "jsons",
+        path: `${__dirname}/static/jsons/`,
+      },
+      __key: "jsons",
+    },
+  ],
 };
 
 export default config;
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
 const postcssNested = require("postcss-nested");
-const postcssSimpleVars = require("postcss-simple-vars");
-const postcssMixins = require("postcss-mixins");
-const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = () => ({
-  plugins: [postcssNested, postcssSimpleVars, postcssMixins, postcssPresetEnv],
+  plugins: [postcssNested],
 });
