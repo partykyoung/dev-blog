9b90dc6690f241e70fc02a4287623a826167504d
partykyoung <partykyoung@gmail.com>
2024-02-25 18:02:01 +0900

    feat: postcss를 사용하여  반응형 퍼블리싱 환경 작업

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
 const autoPrefixer = require("autoprefixer");
 const postcssNested = require("postcss-nested");
+const postcssMixins = require("postcss-mixins");
+const postcssSimpleVars = require("postcss-simple-vars");
+const postcssImport = require("postcss-import");
+
+const mixins = require("./config/postcss/mixin");
+const variables = require("./config/postcss/variables");
 
 module.exports = () => ({
-  plugins: [autoPrefixer, postcssNested],
+  plugins: [
+    postcssImport,
+    autoPrefixer,
+    postcssSimpleVars({
+      variables,
+    }),
+    postcssMixins({
+      mixins,
+    }),
+    postcssNested,
+  ],
 });
