cf8c3d8f46d180e9ca367b51f9f1f537a8c1c9ad
partykyoung <partykyoung@gmail.com>
2024-02-25 18:02:01 +0900

    feat: postcss를 사용하여  반응형 퍼블리싱 환경 작업

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const autoPrefixer = require('autoprefixer');
 const postcssNested = require('postcss-nested');
 const postcssMixins = require('postcss-mixins');
@@ -9,6 +10,20 @@ const variables = require('./config/postcss/variables');
 
 module.exports = () => ({
   plugins: [
+=======
+const autoPrefixer = require("autoprefixer");
+const postcssNested = require("postcss-nested");
+const postcssMixins = require("postcss-mixins");
+const postcssSimpleVars = require("postcss-simple-vars");
+const postcssImport = require("postcss-import");
+
+const mixins = require("./config/postcss/mixin");
+const variables = require("./config/postcss/variables");
+
+module.exports = () => ({
+  plugins: [
+    postcssImport,
+>>>>>>> 9b90dc6 (feat: postcss를 사용하여  반응형 퍼블리싱 환경 작업)
     autoPrefixer,
     postcssSimpleVars({
       variables,
@@ -17,10 +32,13 @@ module.exports = () => ({
       mixins,
     }),
     postcssNested,
+<<<<<<< HEAD
     postcssPresetEnv({
       features: {
         'nesting-rules': true,
       },
     }),
+=======
+>>>>>>> 9b90dc6 (feat: postcss를 사용하여  반응형 퍼블리싱 환경 작업)
   ],
 });
