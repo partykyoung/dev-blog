8dd76c479aea5e28ade6a40aad9484f9d4346a73
partykyoung <partykyoung@gmail.com>
2024-03-01 02:28:00 +0900

    feat: button 컴포넌트 작업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,20 @@ import { DefaultLayout } from "../widgets/default-layout";
 
 import { HomePosts } from "../widgets/home-posts";
 import { Container } from "../shared/uis/container";
+import { Button } from "../shared/uis/button";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <DefaultLayout>
       <Container>
         <HomePosts />
+        <Button variant="fill" color="primary">
+          그래
+        </Button>
+        <Button variant="outline" color="primary">
+          사랑은
+        </Button>
+        <Button>돌아오는거야</Button>
       </Container>
     </DefaultLayout>
   );
