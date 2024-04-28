3c8b53c86826857f87c35cccfe25027251128f36
partykyoung <partykyoung@gmail.com>
2024-04-28 18:36:06 +0900

    fix: 개발모드에서 재빌드시 json 파일 재생성 떄문에 무한루프 걸리는 이슈 수정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,4 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -10,18 +10,18 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    "gatsby-plugin-postcss",
+    'gatsby-plugin-postcss',
     // "gatsby-plugin-google-gtag", FIXME: 리팩토링 끝나고 나서 애널리틱스 계정 생성 후 연동 필요
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
         icon: `${__dirname}/static/images/icon.png`,
       },
     },
     {
-      resolve: "gatsby-plugin-mdx",
+      resolve: 'gatsby-plugin-mdx',
       options: {
         extensions: [`.mdx`, `.md`],
         gatsbyRemarkPlugins: [
@@ -34,50 +34,54 @@ const config: GatsbyConfig = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: "language-",
+              classPrefix: 'language-',
             },
           },
         ],
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
+        ignore: [`${__dirname}/static/jsons/`],
       },
-      __key: "pages",
+      __key: 'pages',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "posts",
+        name: 'posts',
         path: `${__dirname}/posts/`,
+        ignore: [`${__dirname}/static/jsons/`],
       },
-      __key: "posts",
+      __key: 'posts',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
+        name: 'images',
         path: `${__dirname}/static/images/`,
+        ignore: [`${__dirname}/static/jsons/`],
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "jsons",
+        name: 'jsons',
         path: `${__dirname}/static/jsons/`,
+        ignore: [`${__dirname}/static/jsons/`],
       },
-      __key: "jsons",
+      __key: 'jsons',
     },
     {
-      resolve: "gatsby-omni-font-loader",
+      resolve: 'gatsby-omni-font-loader',
       options: {
-        enableListener: "",
+        enableListener: '',
         preconnect: [
           `https://fonts.googleapis.com`,
           `https://fonts.gstatic.com`,
@@ -88,8 +92,8 @@ const config: GatsbyConfig = {
             file: `https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&display=swap`,
           },
           {
-            name: "Fira Code",
-            file: "https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap",
+            name: 'Fira Code',
+            file: 'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap',
           },
         ],
       },
