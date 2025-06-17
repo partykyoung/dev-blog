9c4e9ff562a7b62dc4ee764046f2332d9d7b3f03
partykyoung <partykyoung@gmail.com>
2025-06-17 22:24:01 +0900

    Updates

---
+++ _next/static/chunks/app/about/page-f97a42b88d210850.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{1907:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[350,552,358],()=>e(1907)),_N_E=_.O()}]);
---
+++ _next/static/chunks/pages/_app-2c9aedc10d06f9d0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4824:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(3962)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4824),n(9156))),_N_E=_.O()}]);
